13c82eb7864b581f4afda7ef272e402ffb943fe9
Nbalboa <nickojek2x@gmail.com>
2021-10-15 12:29:14 +0800

    Data Structure

---
+++ DataStructure/CustomArrayList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace DataStructure
+{
+    public class CustomArrayList<T>
+    {
+        private T[] arr;
+        private int count;
+        private const int INITIAL_CAPACITY = 4;
+        public int Count
+        {
+            get { return this.count; }
+        }
+        public CustomArrayList(int capacity = INITIAL_CAPACITY)
+        {
+            this.arr = new T[capacity];
+            this.count = 0;
+        }
+        public void Add(T item)
+        {
+            GrowIfArrIsFull();
+            this.arr[this.count] = item;
+            this.count++;
+        }
+        public void Insert(int index, T item)
+        {
+            if (index > this.count || index < 0)
+            {
+                throw new IndexOutOfRangeException(
+                    "Invalid index: " + index);
+            }
+            GrowIfArrIsFull();
+            Array.Copy(this.arr, index,
+                this.arr, index + 1, this.count - index);
+            this.arr[index] = item;
+            this.count++;
+        }
+        private void GrowIfArrIsFull()
+        {
+            if (this.count + 1 > this.arr.Length)
+            {
+                T[] extendedArr = new T[this.arr.Length * 2];
+                Array.Copy(this.arr, extendedArr, this.count);
+                this.arr = extendedArr;
+            }
+        }
+        public void Clear()
+        {
+            this.arr = new T[INITIAL_CAPACITY];
+            this.count = 0;
+        }
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.arr.Length; i++)
+            {
+                if (object.Equals(item, this.arr[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public bool Contains(T item)
+        {
+            int index = IndexOf(item);
+            bool found = (index != -1);
+            return found;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Invalid index: " + index);
+                }
+                return this.arr[index];
+            }
+            set
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Invalid index: " + index);
+                }
+                this.arr[index] = value;
+            }
+        }
+        public T RemoveAt(int index)
+        {
+            if (index >= this.count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "Invalid index: " + index);
+            }
+            T item = this.arr[index];
+            Array.Copy(this.arr, index + 1,
+                this.arr, index, this.count - index - 1);
+            this.arr[this.count - 1] = default(T);
+            this.count--;
+            return item;
+        }
+        public int Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index != -1)
+            {
+                this.RemoveAt(index);
+            }
+            return index;
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomArrayList<string> shoppingList = new CustomArrayList<string>();
+            shoppingList.Add("Milk");
+            shoppingList.Add("Honey");
+            shoppingList.Add("Olives");
+            shoppingList.Add("Water");
+            shoppingList[2] = "A lot of " + shoppingList[2];
+            shoppingList.Add("Fruits");
+            shoppingList.RemoveAt(0);
+            shoppingList.RemoveAt(2);
+            shoppingList.Add(null);
+            shoppingList.Add("Beer");
+            shoppingList.Remove(null);
+            Console.WriteLine("We need to buy:");
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(" - " + shoppingList[i]);
+            }
+            Console.WriteLine("Position of 'Beer' = {0}", shoppingList.IndexOf("Beer"));
+            Console.WriteLine("Position of 'Water' = {0}", shoppingList.IndexOf("Water"));
+            Console.WriteLine("Do we have to buy Bread? " + shoppingList.Contains("Bread"));
+        }
+    }
+}
